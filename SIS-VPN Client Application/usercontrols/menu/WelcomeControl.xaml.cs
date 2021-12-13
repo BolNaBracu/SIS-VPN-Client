afdc90693fa59a49a91061ebfd83ea0b3bc25673
Mislav Matijević <mislav.mim@gmail.com>
2021-12-13 23:23:30 +0100

    [VPNSIS-13] Basic user controls Added new basic user controls, one for side menu and other for welcome screen. NOTE: Welcome screen is bad resolution. Should be fixed.

---
+++ SIS-VPN Client Application/usercontrols/menu/WelcomeControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SIS_VPN_Client_Application.usercontrols.menu
+{
+    /// <summary>
+    /// Interaction logic for WelcomeControl.xaml
+    /// </summary>
+    public partial class WelcomeControl : UserControl
+    {
+        public WelcomeControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
